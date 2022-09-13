7bd0f0967e806c30afca2086030a86be80696501
Igor Sverchkov <knjaz1989@gmail.com>
2022-09-13 10:49:37 +0300

    additional _sql_task_3 was changed

--- sql_task_3_additional/task_3_pro_postgres_create.sql
+++ sql_task_3_additional/task_3_pro_postgres_create.sql
@@ -1,13 +1,8 @@
 CREATE TABLE employees (
 	id serial primary key,
 	name varchar(100) NOT NULL,
-	department_id integer references departments(id)
-);
-
-CREATE TABLE directors_employess (
-	director_id integer references employees(id),
-	employee_id integer references employees(id),
-	CONSTRAINT director_employee_pk PRIMARY KEY (director_id, employee_id)
+	department_id integer references departments(id),
+	director_id integer references employees(id)
 );
 
 CREATE TABLE departments (
@@ -16,17 +11,3 @@ CREATE TABLE departments (
 	director_id integer references employees(id)
 );
 
-
-
-
-
-
-DROP TABLE employees, director_employess;
-
-INSERT INTO employees (name, department) VALUES ('vs', 'python');
-
-SELECT * FROM employees;
-
-INSERT INTO director_employess VALUES (1, 1);
-
-SELECT * FROM director_employess;
--- sql_task_3_additional/task_3_simple_postgres_create.sql
+++ sql_task_3_additional/task_3_simple_postgres_create.sql
@@ -2,10 +2,6 @@ CREATE TABLE employees (
 	id serial primary key,
 	name varchar(100) NOT NULL,
 	department varchar(150) NOT NULL
+	director_id integer references employees(id)
 );
 
-CREATE TABLE directors_employess (
-	director_id integer references employees(id),
-	employee_id integer references employees(id),
-	CONSTRAINT director_employee_pk PRIMARY KEY (director_id, employee_id)
-);
